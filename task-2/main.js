e6284d83f8c95c9b259af1357dd425afdd836015
Denys Semenov <54813799+DioCode@users.noreply.github.com>
2019-11-02 11:49:24 +0200

    fixes

--- task-2/main.js
+++ task-2/main.js
@@ -1,7 +1,11 @@
 let str = prompt('Enter a string');
 
-if (str[0] == ' ') {
-  console.log(str.slice(1, str.length - 1));
+if (str[0] == ' ' && str[str.length - 1] == ' ') {
+  console.log(str.slice(1, -1))
+} else if (str[0] == ' ') {
+  console.log(str.slice(1))
 } else if (str[str.length - 1] == ' ') {
-  console.log(str.slice(0, str.length - 1));
-} 
+  console.log(str.slice(0, -1))
+} else {
+  console.log(str);
+}
--- task-3/main.js
+++ task-3/main.js
@@ -1,8 +1,8 @@
 let str = prompt('Write some text');
 let substr = prompt('Write another some text');
 
-if (str[0] == substr[0]) {
-  console.log(true);
+if (str.slice(0, substr.length) == substr) {
+  console.log(true)
 } else {
-  console.log(false);
+  console.log(false)
 }
